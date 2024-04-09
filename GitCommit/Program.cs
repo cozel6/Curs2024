a90fcfb93ed1cf8b32b735efb7c5b26e0ae99c1e
Cozel <Cozel@DESKTOP-ETQ7VGK>
2024-04-09 20:37:42 +0300

    New

--- GitCommit/Program.cs
+++ GitCommit/Program.cs
@@ -4,5 +4,6 @@
     {
         Console.WriteLine("Hello, My First Git Project!");
         Console.WriteLine("Hello, from my new Branch");
+        Console.WriteLine("These are my new changes");
     }
 }
