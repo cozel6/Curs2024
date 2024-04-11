6865a2b11365945b5411bfac58ebcd2fc55c3efc
Cozel <Cozel@DESKTOP-ETQ7VGK>
2024-04-11 19:17:20 +0300

    Manipulation String

--- Strings/Strings/Program.cs
+++ Strings/Strings/Program.cs
@@ -1,4 +1,6 @@
-﻿public static class Program
+﻿using System.Security.Cryptography.X509Certificates;
+
+public static class Program
 {
     public static void Main(string[] args)
     {
@@ -7,16 +9,34 @@
         // String = secvente de carctere / vector / 
         // String object contain immutable (read only) nu l mai poti modifica / reffrenece type
 
-        //Declare string
+        #region Declare string
 
         string myFirstString = "Hello World";
         Console.WriteLine(myFirstString);
 
         int myNumber = 42;
-        Console.WriteLine(myNumber.ToString());    
+        Console.WriteLine(myNumber);    
 
         string mynumberAsString = myNumber.ToString();
+        #endregion
 
+
+
+
+        #region manipulating strigs
+
+        string firstName = "Ionel";
+        string lastName = "Popescu";
+
+        string fullName = firstName + " " + lastName;
+        Console.WriteLine(fullName);
+
+        string nameFullConcat = String.Concat(firstName," ",lastName);
+        Console.WriteLine(nameFullConcat);
+
+        //interpolare
+        Console.WriteLine($"{firstName} {lastName}");
+        #endregion
 
 
 
