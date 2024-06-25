81e04893a06fb1fd82af142166d38c9edff32d6a
Cozel <Cozel@DESKTOP-ETQ7VGK>
2024-06-25 20:56:26 +0300

    basic api endpoints

---
+++ FirstAppi/FirstAppi/Controllers/MoviesContoller.cs
@@ -0,0 +1,69 @@
+﻿using FirstAppi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace FirstAppi.Controllers
+{
+    [Route("api/[controller]")] // api/movies
+    [ApiController]
+    public class MoviesContoller : ControllerBase
+    {
+        private static List<Movie>? movies;
+        public MoviesContoller()
+        {
+            movies = new List<Movie>
+            {
+                new Movie {Id = 1, Title = "Inception", ReleaseDate = DateTime.Now, Duration = 150, Rating = 4,},
+                new Movie {Id = 2, Title = "Back to the future", ReleaseDate = DateTime.MinValue, Duration = 180, Rating = 8,}
+            };
+        }
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            // returns all movies
+            return Ok(movies);
+        }
+        [HttpPost] // api/movies
+        public IActionResult Create()
+        {
+
+            return Ok(movies);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id) //does that entity with id exists ?  if no retrun not found , if yes retrun ok
+        {
+            var existingMovie = movies?.FirstOrDefault(movie => movie.Id == id);
+            if (existingMovie == null)
+            {
+                return NotFound();
+            }
+            return Ok(existingMovie);
+
+        }
+        [HttpHead("{id}")]
+
+
+        public IActionResult CheckIfExists(int id)
+        {
+            if (movies.Any(c => c.Id == id))
+            {
+                return Ok(movies);
+            }
+            return BadRequest();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteOperation(int id)
+        {
+            var moveiWithId = movies.FirstOrDefault(m => m.Id == id);
+            if(moveiWithId == null)
+            {
+                return NotFound();
+            }
+            movies.Remove(moveiWithId);
+            return NoContent();
+        }
+    }
+}
---
+++ FirstAppi/FirstAppi/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FirstAppi.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        [Required]
+        public string? Title { get; set; }
+        public DateTime ReleaseDate{ get; set; }
+
+        public double Rating { get; set; }
+
+        public ushort Duration { get; set; }
+    }
+}
